1094252529baec3b798df8e1355caa691312479d
Osaragii <dalaidev9@gmail.com>
2025-09-05 06:38:21 +0530

    Done setting up authentication for users

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 
 import { BrowserRouter as Router } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 
 const client = new QueryClient();
@@ -12,9 +13,11 @@ const client = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
-      <Router>
-        <App />
-      </Router>
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
